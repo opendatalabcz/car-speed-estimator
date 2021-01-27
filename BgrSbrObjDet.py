3c01798b55aa52eca373c566c1c703c745291da7
Krkosja1 <ibrahim.inkognito@gmail.com>
2021-01-27 20:23:24 +0100

    27.1.2020

--- BgrSbrObjDet.py
+++ BgrSbrObjDet.py
@@ -4,27 +4,27 @@ from PointManager import manager
 import time
 
 
-kernel_dil = np.ones((20, 20), np.uint8)
+kernel_dil = np.ones((5, 5), np.uint8)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
 fgbg = cv2.createBackgroundSubtractorMOG2()
-cap = cv2.VideoCapture("Data/cam_2.mp4")
+cap = cv2.VideoCapture("Data/highway.mp4")
 
 _, frame = cap.read()
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
 old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+out = cv2.VideoWriter("outpy.avi", cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width, frame_height))
 
 lk_params = dict(winSize = (15, 15), maxLevel = 4,
                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-point_selected = False
-point = ()
-old_points2 = manager()
+fshape = frame.shape
+old_points = manager(fshape)
 
 def select_point(event, x, y, flags, params):
-    global point, point_selected, old_points2
+    global old_points
     if event == cv2.EVENT_LBUTTONDOWN:
-        point = (x, y)
-        point_selected = True
-        old_points2.add_point(x, y)
+        old_points.add_point(x, y)
 
 cv2.namedWindow("Frame")
 cv2.setMouseCallback("Frame", select_point)
@@ -38,26 +38,31 @@ while True:
     if ret == True:
         fgmask = fgbg.apply(frame)
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-        dilation = cv2.dilate(fgmask, kernel_dil, iterations = 1)
-        (contours, hierarchy) = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#        dilation = cv2.dilate(fgmask, kernel_dil, iterations = 1)
+        erosion = cv2.erode(fgmask,kernel_dil,iterations = 1)
+        dilation = cv2.dilate(erosion, kernel_dil, iterations=1)
+        cv2.imshow("dilitation", fgmask)
+#        (contours, hierarchy) = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        (contours, hierarchy) = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         for pic, contour in enumerate(contours):
             area = cv2.contourArea(contour)
-            if(area > 3500):
+            if(area > 2500):
                 x, y, w, h = cv2.boundingRect(contour)
                 img = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                old_points.add_point(x + int(w/2), y + int(h/2))
 #                cv2.circle(frame, (x + int(w/2), y + int(h/2)), 5, (0, 255, 0), -1)
 
-    if point_selected:
-        new_points, status, error = cv2.calcOpticalFlowPyrLK(old_gray, gray_frame, old_points2.getAll(), None, **lk_params)
+    if old_points.point_selected():
+        new_points, status, error = cv2.calcOpticalFlowPyrLK(old_gray, gray_frame, old_points.getAll(), None, **lk_params)
         old_gray = gray_frame.copy()
         position = 0
+        old_points.rewrite(new_points)
         for point in new_points:
             x, y = point.ravel()
-            old_points2.rewrite(position,x,y)
-            position = position + 1
             cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
 
     cv2.imshow("Frame", frame)
+    out.write(frame)
     key = cv2.waitKey(1)
     if key == 27:
         break
